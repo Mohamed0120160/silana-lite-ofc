8f97fb2d360b9b893b16d657e9ebd0a729ca5b1f
Mohamed0120160 <142312063+Mohamed0120160@users.noreply.github.com>
2026-06-13 10:35:54 +0300

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '201281392362',
+ pairingNumber : '201281392362',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
+ nomorwa : '201281392362',
  nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomerown : '201201609786',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
